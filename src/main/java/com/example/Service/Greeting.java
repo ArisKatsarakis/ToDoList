6e8ace5d71dd830337e02ec523abcd29077ca84e
KatsarakisAris <aris.katsarakis@repathsolutions.com>
2023-05-31 16:44:52 +0300

    Add Spring Boot basic backend code in repository

---
+++ src/main/java/com/example/Service/Greeting.java
@@ -0,0 +1,4 @@
+package com.example.Service;
+
+
+public record Greeting(long id, String content) { }
---
+++ src/main/java/com/example/todolist/ToDoLIstApplication.java
@@ -0,0 +1,18 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+
+@SpringBootApplication
+@ComponentScan("com.example.Service")
+public class ToDoLIstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoLIstApplication.class, args);
+    }
+
+}
