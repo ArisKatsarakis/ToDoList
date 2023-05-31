2ba52074d1cfc5b2ac080e32c21fa0b70bb4ea9f
KatsarakisAris <ariskatsarakis@hotmail.com>
2023-05-31 16:51:28 +0300

    be done using git cli

--- src/main/java/com/example/Service/GreetingController.java
+++ src/main/java/com/example/Service/GreetingController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class GreetingController {
 
-    private static final String template = "Hello, %s! This is Spring booot";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
 
